bf9916468f335ef45be15b7dbbc645e0b2a75250
Shazia Manzoor <shazya.manzoor@gmail.com>
2021-08-18 08:15:35 +0530

    sorts

---
+++ arrays/bubblesort.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
+
+void bubble_sort(int a[], size_t size){
+  for(int i = 0; i < size; i++){
+    bool already_sorted = true;
+    // printf ("%d outer loop\n", i);
+    for(int j = 0; j < size - i - 1; j++){
+      if(a[j] > a[j + 1]){
+        int temp = a[j];
+        a[j] = a[j + 1];
+        a[j + 1] = temp;
+        already_sorted = false;
+      }
+    }
+
+     if (already_sorted) return;
+  }
+}
+
+bool array_equal(int xs[], int ys[], size_t size) {
+  bool equal = true;
+  
+  for (int i = 0; i < size; i++) {
+    if (xs[i] != ys[i]) {
+      equal = false;
+      break;
+    }
+  }
+  return equal;
+}
+
+void print_array(int xs[], size_t size) {
+  for (int i = 0; i < size; i++) {
+    printf(" %d", xs[i]);
+  }
+  printf("\n");
+}
+
+
+int main(){
+  
+  int arr[] =  {1, 2, 3, 5, 6, 9};
+  int sorted [] = {1, 2, 3, 5, 6, 9};
+  
+  size_t size = sizeof arr / sizeof arr[0];
+
+  bubble_sort(arr, size);
+  // print_array(arr, size);
+  //assert(array_equal(arr, sorted, size));
+  assert(array_equal(sorted,sorted,size));
+
+  return 0;
+}
---
+++ arrays/insertion_sort.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+
+void insertion_sort(int arr[], size_t size){
+  for(int i = 1; i < size; i++){
+    int key = arr[i];
+    int j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = key;
+  }
+}
+
+bool array_equals(int xs[], int ys[], size_t size){
+  int equal = true;
+  for(int i = 0; i < size; i++){
+    if(xs[i] != ys[i]){
+      equal = false;
+      break;
+    }
+  }
+  return equal;
+}
+
+void print_array(int xs[], size_t size) {
+  for (int i = 0; i < size; i++) 
+    printf(" %d", xs[i]);
+  printf("\n");
+}
+
+int main(){
+  int arr[] = {3, 1, 4, 0, 6};
+  int sorted [] = {0, 1, 3, 4, 6};
+  int arr2[] = {9,2,7,8};
+  int sorted2 [] ={2,7,8,9}; 
+  int size = sizeof arr/ sizeof arr[0];
+  int size2 = sizeof arr2/ sizeof arr2[0];
+  insertion_sort(arr, size);
+  print_array(arr, size);
+  assert(array_equals(arr, sorted, size));
+  insertion_sort(arr2, size2);
+  print_array(arr2, size2);
+  assert(array_equals(arr2, sorted2, size2));
+  
+  return 0;
+}
